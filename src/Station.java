ac09a98afad1dc6ca8a5918f01ea08db7f26464e
MuZhenWei <muzhenweizh@gmail.com>
2019-03-24 15:59:33 +0800

    test of writing some classes

---
+++ src/Station.java
@@ -0,0 +1,13 @@
+public class Station {
+    char stationName;       // the name of the station (A, B, C)
+    boolean[] slot;         // the condition of every slots in this station (0---there is no scooter in this slots, 1---there is a scooter in this slot)
+
+    void pickUp(){
+
+    }
+
+    void returnScooter(){
+
+    }
+
+}
---
+++ src/User.java
@@ -0,0 +1,7 @@
+public class User {
+    int QMNo;           // qm number
+    String fName;       // first name
+    String lName;       // last name
+    String email;       // email address
+    boolean fineOrNot;  // does this user need to pay the fine first? (0---no, 1---yes)
+}
